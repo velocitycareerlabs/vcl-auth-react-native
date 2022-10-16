431ae80abe080925bfa06d807ea7154d7e885ef7
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2022-10-16 14:21:31 +0300

    apache copyright header (#3) (#4)

--- ios/VclAuthReactNative.swift
+++ ios/VclAuthReactNative.swift
@@ -1,3 +1,12 @@
+//
+//  VCLAuth.swift
+//  CocoaAsyncSocket
+//
+//  Created by Michael Avoyan on 22/05/2022.
+//
+//  Copyright 2022 Velocity Career Labs inc.
+//  SPDX-License-Identifier: Apache-2.0
+
 import VCLAuth
 
 @objc(VclAuthReactNative)
--- src/api/Dictionary.tsx
+++ src/api/Dictionary.tsx
@@ -1,3 +1,10 @@
+/**
+ * Created by Michael Avoyan on 19/05/2022.
+ *
+ * Copyright 2022 Velocity Career Labs inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export type Dictionary<T> = {
   [key: string]: T;
 };
