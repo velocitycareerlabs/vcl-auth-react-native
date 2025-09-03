cf6fee3c4fb04d29730f367b03277bcca4d158db
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-09-03 14:58:52 +0300

    VL-8834-import-swift-header

--- ios/VclAuthReactNative.mm
+++ ios/VclAuthReactNative.mm
@@ -1,15 +1,18 @@
 #import "VclAuthReactNative.h"
+#import <objc/message.h>
 
 // Import the auto-generated Swift interface header
-#if __has_include("VclAuthReactNative-Swift.h")
+#if __has_include(<VclAuthReactNative/VclAuthReactNative-Swift.h>)
+#import <VclAuthReactNative/VclAuthReactNative-Swift.h>
+#elif __has_include("VclAuthReactNative-Swift.h")
 #import "VclAuthReactNative-Swift.h"
+#else
+#warning "VclAuthReactNative-Swift.h not found. Ensure the Swift sources are compiled and the header is exposed."
 #endif
 
 @implementation VclAuthReactNative
 
 //RCT_EXPORT_MODULE()
-
-// Be explicit so JS and native names
 RCT_EXPORT_MODULE(VclAuthReactNative)
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
